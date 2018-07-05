965bf6d8e49fa0dcc54ae6959cfdd57a1f8ad47d
Qsouler <lll123456ly@163.com>
2018-07-05 15:41:35 +0800

    commit new file is pyt1.py

---
+++ pyt1.py
@@ -0,0 +1,63 @@
+print("Hello World!")
+
+#注释
+
+a = 100
+b = 'f'
+c = "fjdkfljd"
+print(a)
+print(b)
+print(c)
+
+#格式化输出
+print("my is %s" %c)
+
+print("my is %d" %a)
+
+print("%s %d" %(c,a))
+
+name = "fjdkl"
+qqNum = 32232
+phone = 12222222
+address = "东北电力大学"
+print("=============================================")
+print("姓名： %s" %name)
+print("QQ: %s" %qqNum)
+print("手机号：%s" %phone)
+print("公司地址：吉林市 %s" %address)
+
+#两种赋值方式
+num1 = 1
+num2 = 2
+
+num3,num4 = 3,4
+
+print(num3)
+print(num4)
+#引申的交换变量
+
+num3,num4 = num4,num3
+
+print(num3)
+print(num4)
+
+#help方法
+help(print)
+
+#输入,输入的内容会统一变成字符串
+
+
+#s = input()
+#fff = input("请输入姓名：")
+
+
+
+#查看数据类型 type(s)
+#print(type(s))
+
+#取整数
+print(20//3)
+print(20/3)
+
+#幂计算
+print(20**3)
